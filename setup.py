043c2e890f23f30ec48bd87798909d8357abaff6
agarwalson02 <agarwalson02@gmail.com>
2026-02-09 10:59:01 +0530

    Venv Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kartikey Agarwal",
+    author_email="agarwalson02@gmail.com",
+    packages=find_packages()
+)
